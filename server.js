97c684c533ac871296cd39420d2ef7dae69214c9
DerrickLui <rick.pantoja@gmlegacyholdings.com>
2026-02-27 18:55:42 +0800

    Fix for Render deployment: use PORT env var, fix merge conflicts in index.html, and add .gitignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set headers for Godot web exports (COOP/COEP)
 app.use((req, res, next) => {
